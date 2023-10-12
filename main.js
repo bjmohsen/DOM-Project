5457d2708b43314ed2b121cfd299d78d62fe4e43
mohsen <mohsenbouajila108@gmail.com>
2023-10-12 22:05:08 +0200

    adding

---
+++ main.js
@@ -0,0 +1,72 @@
+var qntt=1; 
+var prix= 10;
+
+//fonction calculer le nomber de click btn + et affichier dans leur case
+
+function compteur() {
+    qntt++;
+    document.getElementById("qnt").value=qntt; 
+    document.getElementById("prix").value=qntt*prix ;  
+}
+document.getElementById("plus").addEventListener("click",compteur); // appel fonction compteur 
+
+//fonction calculer le nomber de click btn - et affichier dans leur case
+
+function compteurM(){
+    if (qntt >1) { //condtion d'arret
+        qntt--;
+        document.getElementById("qnt").value=qntt;
+        document.getElementById("prix").value=qntt*prix ; 
+    } 
+    else {
+        alert ("a9al 7aja 1") //msg condtion d'arret
+    }
+}
+document.getElementById("moin").addEventListener("click",compteurM); //app fonction
+
+// les fonction de 2eme articl
+var qntt1=1;
+var prix1= 25;
+function compteur1() {
+    qntt1++;
+    document.getElementById("qnt1").value=qntt1; 
+    document.getElementById("prix1").value=qntt1*prix1 ;  
+}
+document.getElementById("plus1").addEventListener("click", compteur1);
+function compteurM1(){
+    if (qntt1 >0) {
+        qntt1--;
+        document.getElementById("qnt1").value=qntt1;
+        document.getElementById("prix1").value=qntt1*prix1 ; 
+    } 
+    else {
+        alert ("a9al 7aja 1")
+    }
+}
+document.getElementById("moin1").addEventListener("click",compteurM1);
+//fontion de 3emm articl
+var qntt2=1;
+var prix2= 30;
+function compteur2() {
+    qntt2++;
+    document.getElementById("qnt2").value=qntt2; 
+    document.getElementById("prix2").value=qntt2*prix2 ;  
+}
+document.getElementById("plus2").addEventListener("click", compteur2);
+function compteurM2(){
+    if (qntt2 >0) {
+        qntt2--;
+        document.getElementById("qnt2").value=qntt2;
+        document.getElementById("prix2").value=qntt2*prix2 ; 
+    } 
+    else {
+        alert ("a9al 7aja 1")
+    }
+}
+document.getElementById("moin2").addEventListener("click",compteurM2);
+
+
+
+function change (){
+    document.getElementById("img").background = red;}
+document.getElementById("img").addEventListener("click", change());
